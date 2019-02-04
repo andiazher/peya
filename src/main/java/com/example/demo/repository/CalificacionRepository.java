20d7e056d4be230b3c6de868d12e2c9f79637011
Andres Diaz <adiazh@clickdelivery.com>
2019-02-04 15:39:22 -0500

    Update in repository

--- src/main/java/com/example/demo/repository/CalificacionRepository.java
+++ src/main/java/com/example/demo/repository/CalificacionRepository.java
@@ -17,8 +17,10 @@ public interface CalificacionRepository extends JpaRepository<Calificacion, Long
 	@Query("SELECT t FROM Calificacion t where t.compra = :compra AND t.eliminado=0")
 	Collection<Calificacion> findBycompra(@Param("compra") Long compra);
 	
-	@Query("SELECT t FROM Calificacion t where t.usuario = :usuario AND t.eliminado=0")
-	Collection<Calificacion> findByusuario(@Param("usuario") Long usuario);
+	@Query("SELECT t FROM Calificacion t where t.usuario = :usuario AND t.eliminado=0 AND t.fecha BETWEEN :fechai AND :fechaf")
+	Collection<Calificacion> findByusuario(@Param("usuario") Long usuario,
+											@Param("fechai") Date fechai,
+											@Param("fechaf") Date fechaf);
 	
 	@Query("SELECT t FROM Calificacion t where t.tienda = :tienda AND t.fecha BETWEEN :fechai AND :fechaf AND t.eliminado=0")
     public Collection<Calificacion> findByTiendaAndFecha(@Param("tienda") Long id, 
